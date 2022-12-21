da553371a51b0757f46f8c9534a48eab5a4eecb8
Evan Richards <tevanrichards@gmail.com>
2022-12-20 21:00:58 -0800

    Initial commit from Create Next App

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,84 @@
+import NextAuth from 'next-auth';
+import { AppProviders } from 'next-auth/providers';
+import Auth0Provider from 'next-auth/providers/auth0';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+let useMockProvider = process.env.NODE_ENV === 'test';
+const {
+  AUTH0_CLIENT_ID,
+  AUTH0_CLIENT_SECRET,
+  AUTH0_ISSUER,
+  NODE_ENV,
+  APP_ENV,
+} = process.env;
+if (
+  (NODE_ENV !== 'production' || APP_ENV === 'test') &&
+  !(AUTH0_CLIENT_ID && AUTH0_CLIENT_SECRET && AUTH0_ISSUER)
+) {
+  console.log('⚠️ Using mocked GitHub auth correct credentials were not added');
+  useMockProvider = true;
+}
+type Auth0Profile = {
+  given_name: string;
+  family_name: string;
+  nickname: string;
+  name: string;
+  picture: string;
+  locale: string;
+  updated_at: string;
+  email: string;
+  email_verified: boolean;
+  iss: string;
+  sub: string;
+  aud: string;
+  iat: number;
+  exp: number;
+  sid: string;
+};
+
+const providers: AppProviders = [];
+if (useMockProvider) {
+  providers.push(
+    CredentialsProvider({
+      id: 'github',
+      name: 'Mocked GitHub',
+      async authorize(credentials) {
+        if (credentials) {
+          const user = {
+            id: credentials.name,
+            name: credentials.name,
+            email: credentials.name,
+          };
+          return user;
+        }
+        return null;
+      },
+      credentials: {
+        name: { type: 'test' },
+      },
+    }),
+  );
+} else {
+  if (!(AUTH0_CLIENT_ID && AUTH0_CLIENT_SECRET && AUTH0_ISSUER)) {
+    throw new Error('Provider secrets must be set');
+  }
+  providers.push(
+    Auth0Provider({
+      clientId: AUTH0_CLIENT_ID,
+      clientSecret: AUTH0_CLIENT_SECRET,
+      issuer: AUTH0_ISSUER,
+      profile(profile: Auth0Profile) {
+        return {
+          id: profile.sid,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
+    }),
+  );
+}
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers,
+});
