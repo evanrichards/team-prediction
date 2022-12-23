d84fdf987b2e5b1b1d0bc1caaa5be98ebf37f8dc
Evan Richards <tevanrichards@gmail.com>
2022-12-22 23:43:02 -0800

    title styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,9 @@ export default function IndexPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="shadow h-screen bg-ctp-base text-ctp-pink">
-        <h1 className="w-screen text-center from-ctp-blue to-ctp-pink">
+        <h1 className="w-screen font-sans text-2xl subpixel-antialiased font-extrabold tracking-wide leading-loose text-center from-ctp-blue to-ctp-mauve">
           Team Prediction
         </h1>
-        <br />
         <div className="flex justify-center">
           <Signup />
         </div>
