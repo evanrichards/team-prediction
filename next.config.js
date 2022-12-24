3fe60c71b443d90b99717e7ae90e3e570c50af41
Evan Richards <tevanrichards@gmail.com>
2022-12-23 16:52:20 -0800

    vercel cors concerns

--- next.config.js
+++ next.config.js
@@ -10,4 +10,30 @@ module.exports = {
     // Will be available on both server and client
     APP_URL: process.env.APP_URL,
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
