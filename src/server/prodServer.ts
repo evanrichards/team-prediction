f3626ea9c1a6d48802d4f9bdb88ca420a8f96442
Evan Richards <tevanrichards@gmail.com>
2022-12-30 14:12:33 -0600

    serverside cors setup

--- src/server/prodServer.ts
+++ src/server/prodServer.ts
@@ -1,11 +1,38 @@
-import http from 'http';
+import http, { IncomingMessage, ServerResponse } from 'http';
 import next from 'next';
+import { NextUrlWithParsedQuery } from 'next/dist/server/request-meta';
 import { parse } from 'url';
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+const allowCors =
+  (fn: typeof handle) =>
+  async (
+    req: IncomingMessage,
+    res: ServerResponse,
+    parsedUrl?: NextUrlWithParsedQuery,
+  ) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
+    if (req.method === 'OPTIONS') {
+      res.statusCode = 200;
+      return;
+    }
+    return await fn(req, res, parsedUrl);
+  };
 
 app.prepare().then(() => {
   const server = http.createServer((req, res) => {
@@ -20,7 +47,7 @@ app.prepare().then(() => {
     }
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const parsedUrl = parse(req.url!, true);
-    handle(req, res, parsedUrl);
+    allowCors(handle)(req, res, parsedUrl);
   });
   server.listen(port);
 
