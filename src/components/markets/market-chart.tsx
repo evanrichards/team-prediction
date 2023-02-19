4b5bb29d433da706a53e4b941c87840257db2539
Evan Richards <tevanrichards@gmail.com>
2023-02-18 22:01:35 -0800

    componentize market page

---
+++ src/components/markets/market-chart.tsx
@@ -0,0 +1,154 @@
+import { LedgerEntry, MarketWithActivity } from 'src/types/market';
+import tw from 'tailwind-styled-components';
+
+import { enUS } from 'date-fns/locale';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+  TimeScale,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import 'chartjs-adapter-date-fns';
+import { marketValueForLedger } from 'src/common/markets/utils';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+  TimeScale,
+);
+export const options = {
+  animation: {
+    duration: 0,
+  },
+  ticks: {
+    source: 'data',
+  },
+  scales: {
+    x: {
+      type: 'time',
+      time: {
+        unit: 'day',
+      },
+    },
+    y: {
+      min: 0,
+      max: 100,
+    },
+  },
+  // turn off legend and title:
+  plugins: {
+    legend: {
+      display: false,
+    },
+    title: {
+      display: false,
+    },
+  },
+  adapters: {
+    date: {
+      locale: enUS,
+    },
+  },
+  responsive: true,
+  maintainAspectRatio: false,
+};
+export default function MarketChart({
+  marketData,
+  ledger,
+}: {
+  marketData: MarketWithActivity;
+  ledger: LedgerEntry[];
+}) {
+  const graphData = createGraphData({ ...marketData, marketLedger: ledger });
+  const graphComponentInput = {
+    datasets: [
+      {
+        fill: true,
+        data: graphData,
+        backgroundColor: '#A6E3A1',
+        borderColor: '#40A02B',
+      },
+    ],
+  };
+  return (
+    <MarketGraphContainer>
+      <Line
+        width={400}
+        height={400}
+        options={{
+          ...options,
+          scales: {
+            ...options.scales,
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            x: {
+              ...options.scales.x,
+              min: new Date(marketData.createdAt).valueOf(),
+              max: marketData.closedAt
+                ? new Date(marketData.closedAt).valueOf()
+                : new Date().valueOf(),
+            },
+          },
+        }}
+        data={graphComponentInput}
+      />
+    </MarketGraphContainer>
+  );
+}
+function createGraphData(market: MarketWithActivity) {
+  const { createdAt, closedAt, marketLedger } = market;
+  if (marketLedger.length === 0) {
+    return [
+      { y: 50, x: new Date(createdAt).valueOf() },
+      { y: 50, x: new Date().valueOf() },
+    ];
+  }
+  // sort market ledger by date
+  marketLedger.sort((a, b) => {
+    return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+  });
+  const data: { y: number; x: number }[] = [];
+  Array(marketLedger.length)
+    .fill(0)
+    .forEach((_, i) => {
+      const value = marketValueForLedger(marketLedger.slice(0, i + 1));
+      data.push({
+        y: value,
+        x: new Date(marketLedger[i].createdAt).valueOf(),
+      });
+    });
+  const currentValue = marketValueForLedger(marketLedger);
+  if (closedAt) {
+    data.push({
+      y: currentValue,
+      x: new Date(closedAt).valueOf(),
+    });
+  } else {
+    data.push({
+      y: currentValue,
+      x: new Date().valueOf(),
+    });
+  }
+
+  return data;
+}
+const MarketGraphContainer = tw.div`
+border-mantle
+rounded
+bg-surface0
+mt-6
+p-6
+`;
---
+++ src/components/markets/market-description.tsx
@@ -0,0 +1,79 @@
+import {
+  filterUserLiveShares,
+  marketValueDisplay,
+} from 'src/common/markets/utils';
+import Button from 'src/components/Button';
+import { LedgerEntry, MarketWithActivity } from 'src/types/market';
+import { UserUuid } from 'src/types/user';
+import tw from 'tailwind-styled-components';
+
+export default function MarketDescription({
+  marketValue,
+  marketData,
+  ledger,
+  handleBuyYes,
+  handleBuyNo,
+  handleSellYes,
+  handleSellNo,
+  mutating,
+  userUuid,
+}: {
+  marketValue: number;
+  marketData: MarketWithActivity;
+  ledger: LedgerEntry[];
+  handleBuyYes: () => void;
+  handleBuyNo: () => void;
+  handleSellYes: () => void;
+  handleSellNo: () => void;
+  mutating: boolean;
+  userUuid: UserUuid;
+}) {
+  const userLiveShares = filterUserLiveShares(ledger, userUuid);
+  return (
+    <MarketTextContainer>
+      <p className="prose text-current">{marketData.description}</p>
+      <div className="flex flex-wrap">
+        <span className="title-font text-2xl font-medium text-rosewater">
+          {marketValueDisplay(marketValue)}
+        </span>
+        <div className="ml-auto flex gap-4 text-white">
+          <Button
+            onClick={handleBuyYes}
+            disabled={mutating || handleBuyYes === undefined}
+          >
+            Buy Yes
+          </Button>
+          <Button
+            onClick={handleBuyNo}
+            disabled={mutating || handleBuyNo === undefined}
+          >
+            Buy No
+          </Button>
+          <Button
+            onClick={handleSellYes}
+            disabled={
+              mutating ||
+              handleSellYes === undefined ||
+              userLiveShares.yesLiveCount < 1
+            }
+          >
+            Sell Yes
+          </Button>
+          <Button
+            onClick={handleSellNo}
+            disabled={
+              mutating ||
+              handleSellNo === undefined ||
+              userLiveShares.noLiveCount < 1
+            }
+          >
+            Sell No
+          </Button>
+        </div>
+      </div>
+    </MarketTextContainer>
+  );
+}
+const MarketTextContainer = tw.div`
+mt-6 w-full lg:mt-0 lg:py-6 lg:pl-10 flex-grow
+`;
